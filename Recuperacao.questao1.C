dab1d5e55e7dbe0b62e2a64f9e64245bc4515d31
Kauepausic06 <pausic@alunos.utfpr.edu.br>
2025-04-24 15:35:34 -0300

    Create Recuperacao.questao1.C

---
+++ Recuperacao.questao1.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+
+    float altura, largura, area, tinta;
+
+    printf("Digite a altura da parede (em metros): ");
+    scanf("%f", &altura);
+
+    printf("Digite a largura da parede (em metros): ");
+    scanf("%f", &largura);
+
+    area = altura * largura;
+    printf("A area total da parede e: %.2f metros quadrados\n", area);
+
+    tinta = area / 2;
+    tinta = ceil(tinta);
+    printf("A quantidade de tinta necessaria e: %.2f litros\n", tinta);
+
+    return 0;
+}
